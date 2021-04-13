4b51025b852a3ea16621d51424a306e7aa20e677
ABC154128 <ABCd154128>
2021-04-13 23:25:04 +0800

    结构体基础

---
+++ g结构函数.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//定义结构体:
+//struct books {
+//	char arr[10];
+//	int price;
+//	int page;
+//
+//};
+//int main() {
+////使用存数据:
+//	struct books s = {
+//		"ffsffsd",
+//		34,
+//		56
+//	};
+////访问数据:
+//	//(1).第一种 :常规法.
+//	printf("%s\n",s.arr);
+//	printf("%d\n",s.price);
+//	printf("%d\n",s.page);
+//	//(2.)用指针方访问
+//	struct books* p = &s;
+//	printf("%s\n", p->arr);
+//	printf("%d\n", p->price);
+//	printf("%d\n",p->page);
+// /*修改值:*/
+//	s.price = 99;
+//	printf("%d", s.price);
+//	p->page = 5666;
+//	printf("%d\n", s.page);
+//
+//
+//	return 0;
+//
+//}
